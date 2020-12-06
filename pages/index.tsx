fb24780d197cffcf4998273e9614964c4b547fb7
Vincent Guillebaud <vincent.guillebaud31@gmail.com>
2020-12-06 17:27:08 +0100

    ✏️ Message

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,6 +28,9 @@ export default function Home({
           que ça va ✨🚀🎃
         </p>
         <p>
+          Hello Typescript 🏃‍♂️🔥
+        </p>
+        <p>
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
